43a67b374ebe34eb7ba3d54adb464516566fc1bf
kwmkade <80997520+kwmkade@users.noreply.github.com>
2022-03-26 10:55:25 +0900

    up

--- app/src/main/java/com/kwmkade/kotlinsamplewebview/MainActivity.kt
+++ app/src/main/java/com/kwmkade/kotlinsamplewebview/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.kwmkade.kotlinsamplewebview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mUrlEditText: EditText
+    private lateinit var mSearchButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mUrlEditText = findViewById(R.id.url_et)
+        mSearchButton = findViewById(R.id.search_btn)
     }
 }
